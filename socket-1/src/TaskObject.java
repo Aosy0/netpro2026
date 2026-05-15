16a3bac2503829c34bd098365be9d855e7b42a2a
Aosy <koboyutaao@gmail.com>
2026-05-15 13:44:11 +0900

    課題5-2 途中

---
+++ socket-1/src/TaskObject.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class TaskObject implements Serializable, ITask {
+  public String message;
+  public String content;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String present) {
+    this.content = present;
+  }
+
+}
