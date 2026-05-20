4fc293b8802174ce871830d7eba88e3d2036ae39
Aosy0 <koboyutaao@gmail.com>
2026-05-21 02:25:29 +0900

    isPrime()を実装

--- socket-1/src/TaskObject.java
+++ socket-1/src/TaskObject.java
@@ -27,10 +27,40 @@ public class TaskObject implements Serializable, ITask {
   }
 
   public void exec() {
-    isPrime(execNumber);
+    // int temp = execNumber;
+    for (int i = execNumber; i >= 2; i--) {
+      if (isPrime(i)) {
+        result = i;
+        return;
+      }
+    }
+    result = 0;
   }
 
   public int getResult() {
     return result;
+  }
+
+  public static boolean isPrime(int n) {
+    if (n <= 1) {
+      return false;
+    }
+    if (n <= 3) {
+      return true;
+    }
+
+    // 2と3の倍数以外の奇数を確認
+    if (n % 2 == 0 || n % 3 == 0) {
+      return false;
+    }
+
+    // 6k ± 1の形の数のみを確認
+    for (int i = 5; i * i <= n; i += 6) {
+      if (n % i == 0 || n % (i + 2) == 0) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
