87ddce83612865d403420b805e6164455d41603a
Aosy <koboyutaao@gmail.com>
2026-05-22 12:29:04 +0900

    課題6-1

--- socket-1/src/networking/udp/UDPClient.java
+++ socket-1/src/networking/udp/UDPClient.java
@@ -3,19 +3,27 @@ package networking.udp;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 public class UDPClient {
   public static void main(String[] args) {
     DatagramSocket socket = null;
     try {
       socket = new DatagramSocket();
-      InetAddress serverAddress = InetAddress.getByName("192.168.68.52");
+      InetAddress serverAddress = InetAddress.getByName("localhost");
       String message = "Hello, server!";
-      byte[] sendData = message.getBytes();
+      byte[] sendData = message.getBytes(StandardCharsets.UTF_8);
+      byte[] receiveData = new byte[1024];
 
       // 送信パケットを作成
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 9876);
       socket.send(sendPacket);
+
+      // サーバからの返信を受信
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      socket.receive(receivePacket);
+      String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+      System.out.println("サーバからの返信: " + serverResponse);
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
--- socket-1/src/networking/udp/UDPServer.java
+++ socket-1/src/networking/udp/UDPServer.java
@@ -2,6 +2,8 @@ package networking.udp;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 public class UDPServer {
   public static void main(String[] args) {
@@ -9,6 +11,7 @@ public class UDPServer {
     try {
       socket = new DatagramSocket(9876);
       byte[] receiveData = new byte[1024];
+      byte[] sendData;
 
       while (true) {
         // 受信パケットを作成
@@ -17,6 +20,15 @@ public class UDPServer {
 
         String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
         System.out.println("受信: " + message);
+
+        InetAddress clientAddress = receivePacket.getAddress();
+        int clientPort = receivePacket.getPort();
+
+        String serverResponse = message.toUpperCase();
+        sendData = serverResponse.getBytes(StandardCharsets.UTF_8);
+        // 返信をクライアントに送信
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+        socket.send(sendPacket);
       }
     } catch (Exception e) {
       e.printStackTrace();
