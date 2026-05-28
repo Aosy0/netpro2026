19b728f636d5da3db42d027a70079b717ea9f98a
Aosy0 <koboyutaao@gmail.com>
2026-05-28 21:23:55 +0900

    課題6-3

--- socket-1/src/thread/CountAZTenRunnable.java
+++ socket-1/src/thread/CountAZTenRunnable.java
@@ -4,27 +4,32 @@ package thread;
 public class CountAZTenRunnable implements Runnable {
   // main メソッドはプログラムのエントリーポイントです。
   public static void main(String[] args) {
+    Thread[] threads = new Thread[26];
     // 2つの文字を初期化します。
-    char c1 = 97; // ASCII値 97 は 'a' です
-    char c2 = (char) (c1 + 1); // c1 に 1 を足すと ASCII値 98 になり、'b' になります
+    char c = 97; // ASCII値 97 は 'a' です
+    for (int i = 0; i < 26; i++) {
+      // 初期化した文字をコンソールに出力します。
+      // System.out.println(c); // 出力: a
+      CountAZTenRunnable ct = new CountAZTenRunnable();
+      threads[i] = new Thread(ct, String.valueOf(c));
+      threads[i].start();
+      c = (char) (c + 1); // c に 1 を足すと次の文字になります
+    }
 
-    // 初期化した文字をコンソールに出力します。
-    System.out.println(c1); // 出力: a
-    System.out.println(c2); // 出力: b
 
     // CountAZTenRunnable クラスのインスタンスを作成します。
-    CountAZTenRunnable ct = new CountAZTenRunnable();
+
 
     // ct を実行する新しいスレッドを作成します。
-    Thread th = new Thread(ct);
+    // Thread th = new Thread(ct);
 
     // スレッドを開始します。これにより、CountAZTenRunnable の run メソッドが呼び出されます。
-    th.start();
+    // th.start();
 
     // この try-catch ブロックは、0 から 9 までの値を 500 ミリ秒間隔で出力するループを実行します。
     try {
       for (int i = 0; i < 10; i++) {
-        System.out.println("main:i=" + i);
+        // System.out.println("main:i=" + i);
 
         // メインスレッドを 500 ミリ秒間一時停止します。
         Thread.sleep(500); // ミリ秒単位のスリープ時間
@@ -40,10 +45,11 @@ public class CountAZTenRunnable implements Runnable {
     // この try-catch ブロックは、0 から 9 までの値を 1000 ミリ秒間隔で出力するループを実行します。
     try {
       for (int i = 0; i < 10; i++) {
-        System.out.println("runnable thread:i=" + i);
+        System.out.println(Thread.currentThread().getName() + i);
+        // System.out.println();
 
         // スレッドを 1000 ミリ秒間一時停止します。
-        Thread.sleep(1000); // ミリ秒単位のスリープ時間
+        Thread.sleep(100); // ミリ秒単位のスリープ時間
       }
     } catch (InterruptedException e) {
       // スレッドが中断された場合は、例外を出力します。
